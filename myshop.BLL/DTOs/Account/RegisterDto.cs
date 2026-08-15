694ce0fdc7b4725f085010fc1c4f18e6733aa4f4
Abdo Othman <abdoetman010@gmail.com>
2026-08-15 15:30:47 +0300

    [ADD] Implement Account-related DTOs including LoginDto and RegisterDto

---
+++ myshop.BLL/DTOs/Account/RegisterDto.cs
@@ -0,0 +1,9 @@
+namespace myshop.BLL.DTOs.Account;
+
+public record RegisterDto
+{
+    public required string Name { get; set; }
+    public string UserName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
