6d44cd816a87c0e6eeceea5317d15cb135816d25
Abdo Othman <abdoetman010@gmail.com>
2026-08-26 18:42:24 +0300

    [EDIT] Update ProductListDto to contains AverageRating, ReviewCount, IsDeleted, CreatedAt, and UpdatedAt properties

--- myshop.BLL/DTOs/Product/ProductListDto.cs
+++ myshop.BLL/DTOs/Product/ProductListDto.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace myshop.BLL.DTOs.Product;
 
 public record ProductListDto : ProductDtoBase
@@ -6,4 +8,9 @@ public record ProductListDto : ProductDtoBase
     public string Img { get; set; } = string.Empty;
     public decimal Price { get; set; }
     public string CategoryName { get; set; } = string.Empty;
+    public double AverageRating { get; set; }
+    public int ReviewCount { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
 }
